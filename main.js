16afe23328d5330a4ae6e631b7256c9c2d71b9a3
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2022-01-17 20:18:15 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 //SCROLL REVEAL JS
 
-/*==================== SHOW SCROLL TOP ====================*/ 
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
-}
-window.addEventListener('scroll', scrollTop)
-
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
     origin: 'top', //nereden nereye
