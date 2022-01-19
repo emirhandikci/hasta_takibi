e3dda13044b6c0bc76deabdb995d3b22fd28d1fe
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2022-01-19 21:45:53 +0300

    header

--- doctor.php
+++ doctor.php
@@ -27,6 +27,11 @@
 </head>
 
 <body>
+    <div class="container1"> 
+        <div class= "row">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000b76" fill-opacity="1" d="M0,64L48,74.7C96,85,192,107,288,133.3C384,160,480,192,576,181.3C672,171,768,117,864,117.3C960,117,1056,171,1152,176C1248,181,1344,139,1392,117.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+        </div>
+    </div>
     <div class="container">
         <div class="row ">
             <form class="d-flex" id="doctorForm">
