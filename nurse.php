8841f378ce58d7d0fa41829da72f651bf2a4224a
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2022-01-19 22:03:40 +0300

    animasyon geri çekme

--- nurse.php
+++ nurse.php
@@ -24,7 +24,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/> -->
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/> -->
 </head>
 
 <body>
@@ -84,7 +84,7 @@
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
     <script src="./main.js"></script>
-        <!-- <script src="https://unpkg.com/scrollreveal"></script> -->
+        <!--<script src="https://unpkg.com/scrollreveal"></script> -->
     <script src="./graphs.js"></script>
 </body>
 
